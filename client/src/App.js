c49c8a6c1b8833cbba32d787fc60cb7dfbf16312
Axe-Pearl <mycoursesbattletour@gmail.com>
2022-04-05 17:49:22 +0530

    Home Page Navbar created (Frontend init)

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Home from "./Pages/home/Home";
+function App() {
+  return (
+    <div className="App">
+     <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/navbar/Navbar.js
@@ -0,0 +1,49 @@
+import React,{useState} from 'react'
+import "./Navbar.css";
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+
+function Navbar() {
+  const [IsScrolled, setIsScrolled] = useState(false);
+  window.onscroll = ()=>{
+      setIsScrolled(window.pageYOffset == 0 ? false : true);
+      return () => (window.onscroll = null);
+  }
+  console.log(IsScrolled);
+  return (
+    <div className = {IsScrolled ? "navbarscrolled" : "navbar"}>
+        <div className="container">
+        <div className="left">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+            alt=""
+          />
+          <span>Home</span>
+          <span>Series</span>
+          <span>Movies</span>
+          <span>New & Popular</span>
+          <span>My List</span>
+        </div>
+        <div className='right'>
+            <SearchIcon className='icon'/>
+            <span>CHILDREN</span>
+            <NotificationsIcon className='icon'/>
+            <img
+            src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+            />
+            <div className='profile'>
+            <ArrowDropDownIcon className='icon'/>
+            <div className="options">
+                <span>settings</span>
+                <span>Logout</span>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
