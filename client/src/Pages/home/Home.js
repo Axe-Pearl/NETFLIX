56c38867e6a0799a96ec1d6227d5f52bf1e89e4b
Axe-Pearl <mycoursesbattletour@gmail.com>
2022-04-06 00:43:06 +0530

    Home page design v2

--- client/src/Pages/home/Home.js
+++ client/src/Pages/home/Home.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import "./Home.css";
 import Navbar from '../../components/navbar/Navbar';
+import Featured from '../../components/featured/Featured';
 function Home() {
   return (
     <div>
       <Navbar />
-      <img className="homeImage" src='https://assets.nflxext.com/ffe/siteui/vlv3/0bd3a69d-6790-4edc-9818-1c8c558946c2/e1a8a32a-a07a-4bae-965a-9b218881763e/IN-en-20220329-popsignuptwoweeks-perspective_alpha_website_large.jpg'></img>
+      <Featured type = "movie"/>
     </div>
   )
 }
