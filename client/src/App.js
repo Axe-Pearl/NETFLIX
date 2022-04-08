1d501de745f39e4d07a44b0fec884e823b354ce9
Axe-Pearl <mycoursesbattletour@gmail.com>
2022-04-08 14:53:16 +0530

    Register Page Created

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Home from "./Pages/home/Home";
+import Register from './Pages/register/Register';
 import Watch from './Pages/watch/Watch';
 function App() {
   return (
     <div className="App">
-     <Home />
+     <Register />
     </div>
   );
 }
