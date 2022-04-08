55cbba34558f941d42007f3a710de6665ed144cd
Axe-Pearl <mycoursesbattletour@gmail.com>
2022-04-08 15:50:27 +0530

    Login Page Created

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import Home from "./Pages/home/Home";
 import Register from './Pages/register/Register';
 import Watch from './Pages/watch/Watch';
+import Login from "./Pages/login/Login";
+
 function App() {
   return (
     <div className="App">
-     <Register />
+     <Home />
     </div>
   );
 }
